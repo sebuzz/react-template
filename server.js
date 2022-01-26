9f127601eecec530924508af87a946b4ce8fe347
Sebastian Künne <sebuzz@mac.com>
2022-01-26 23:27:35 +0100

    feat: added Database file and uuid, POST method to add todos

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 import express from "express";
-
-// const express = require('express')
+import { readFile, writeFile } from "fs/promises";
+import { v4 as uuid } from "uuid";
 
 const app = express();
+app.use(express.json());
 const port = 3025;
 
-const jason = {
-	Todos: [
-		{
-			name: "Sebastian",
-			city: "Hamburg",
-		},
-		{
-			name: "Florian",
-			city: "Frankfurt",
-		},
-	],
-};
-app.get("/", (req, res) => {
-	res.send("Hello World");
+const DATABASE_URI = "./database/database.json";
+
+app.get("/", (request, response) => {
+	response.send("Hello World");
 });
 
-app.get("/todos/", (request, response) => {
-	response.json(jason); // JSON.stringify(jason)
+app.get("/todos/", async (request, response) => {
+	const data = await readFile(DATABASE_URI, "utf8");
+	const json = JSON.parse(data);
+	console.log(json);
+	response.json(json.todos); // JSON.stringify(jason)
+});
+
+app.post("/todos/", async (request, response) => {
+	const data = await readFile(DATABASE_URI, "utf8");
+	const json = JSON.parse(data);
+	console.log(request.body);
+	const todo = {
+		...request.body,
+		id: uuid(),
+	};
+	json.todos.push(todo);
+	await writeFile(DATABASE_URI, JSON.stringify(json));
+	response.status(201);
+	response.json(todo);
 });
 
 app.listen(port, () => {
